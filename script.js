e64e43174dfc33bfa430a7c449962ee3d0417623
Fiuzah <isabelle.fiuza@escola.pr.gov.br>
2026-06-19 18:53:20 -0300

    botao A e A- adicionados

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 console.log("Conecta+ iniciado!");
 
-
+/* ----------------------BOTAO A+ ------------------------------ */
 const tamanho = document.getElementById("tamanho");
 
-let fonte = 16;
 
 tamanho.addEventListener("click", aumentarFonte);
 
 function aumentarFonte(){
-alert("clicou");
+
     document.querySelectorAll("p, h1, h2").forEach(texto => {
 
         let tamanhoAtual = parseInt(
@@ -16,6 +15,36 @@ alert("clicou");
         );
 
         texto.style.fontSize = (tamanhoAtual + 2) + "px";
+    });
+
+}
+/* -------------------------BOTAO A- --------------------------- */
+const diminuir = document.getElementById("diminuir");
+
+diminuir.addEventListener("click", diminuirFonte);
+
+function diminuirFonte(){
+
+    document.querySelectorAll("p, h1, h2").forEach(texto => {
+
+        let tamanhoAtual = parseInt(
+            window.getComputedStyle(texto).fontSize
+        );
+
+        texto.style.fontSize = (tamanhoAtual - 2) + "px";
+    });
+
+}
+/* ------------------------BOTAO A ---------------------------- */
+const resetar = document.getElementById("resetar");
+
+resetar.addEventListener("click", resetarFonte);
+
+function resetarFonte(){
+
+    document.querySelectorAll("p, h1, h2").forEach(texto => {
+
+        texto.style.fontSize = "";
 
     });
 
